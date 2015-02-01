ef7341cdfe549fa3e718e67496113bf86c5f4514
Slava Shklyaev <shk.slava@gmail.com>
2015-02-01 15:10:15 +0200

    Add keyboard

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber = false
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        println("digit = \(digit)")
+        display.text = userIsInTheMiddleOfTypingANumber ? display.text! + digit : digit
+        userIsInTheMiddleOfTypingANumber = true
+    }
 }
-
